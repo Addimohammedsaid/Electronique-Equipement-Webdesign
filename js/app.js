bd1c057582a0faeb96b0437549494f99a3e6c67e
Addi Mohammed Said <Addi_Mohammed_Said@outlook.fr>
2020-08-10 08:30:43 +0100

    update home page

--- js/app.js
+++ js/app.js
@@ -1,25 +1,5 @@
-// slide logic
-const categoriesBox = document.getElementsByClassName("category-box");
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function showSlides(n) {
-  slideIndex = n;
-  if (n >= categoriesBox.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = categoriesBox.length;
-  }
-  for (i = 0; i < categoriesBox.length; i++) {
-    categoriesBox[i].setAttribute("style", "display: none;");
-  }
-  for (i = n; i < n + 3; i++) {
-    categoriesBox[i].setAttribute("style", "display: block;");
-  }
-}
+var nav = document.getElementById("header");
+window.onscroll = function () {
+  console.log(nav.scrollHeight);
+  nav.classList.toggle("scrolled", document.scrollingElement.scrollTop > 50);
+};
