9d9ab9ae3efa28eff480027e1a1b10fd8c06b5f9
Addi Mohammed Said <Addi_Mohammed_Said@outlook.fr>
2020-08-13 11:33:46 +0100

    homepage responsive

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ window.onscroll = function () {
   console.log(nav.scrollHeight);
   nav.classList.toggle("scrolled", document.scrollingElement.scrollTop > 50);
 };
+
+var menu1 = document.getElementById("menu1");
+
+menu1.onclick = function () {
+  document.getElementById("sub1").classList.toggle("show");
+};
